7bd03eaa5c5918e1bf8b1b39d8af00203ec2185f
Jhosim-agudelo <jhosim.alejandro.agudelo.campo@umontreal.ca>
2023-03-27 23:33:40 -0400

    tache 2 F1 version 1

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,33 +1,73 @@
 package client;
 
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import server.models.Course;
+
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
+
 
     static String messagePrincipal = "*** Bienvenue au portail d'inscription de cours de l'UDEM***\n" +
             "Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours:\n" +
             "1. Automne\n2. Hiver\n3. Ete";
 
-    public static void main(String[] args){
-        try {
-            Socket clientSocket = new Socket("127.0.0.1",1337);
-            OutputStreamWriter os = new OutputStreamWriter(clientSocket.getOutputStream());
-            BufferedWriter writer = new BufferedWriter(os);
-            System.out.println(messagePrincipal);
 
+    public static void affichageDesCours(ObjectOutputStream os,
+                                  ObjectInputStream is,
+                                  Scanner scan) throws IOException, ClassNotFoundException {
+        System.out.println(messagePrincipal);
 
+        while (scan.hasNext()) {
+            String answer = scan.nextLine();
+            switch (answer) {
+                case "1":
+                    os.writeObject("CHARGER Automne");
+                    break;
+                case "2":
+                    os.writeObject("CHARGER Hiver");
+                    break;
+                case "3":
+                    os.writeObject("CHARGER Ete");
+                    break;
+            }
 
+            ArrayList<Course> listOfCourses = (ArrayList<Course>) is.readObject();
+            String session = listOfCourses.get(0).getSession();
+            System.out.println("Les cours offerts pendant la session d'"+session+" sont:");
 
-
-        }catch (IOException ex){
-            ex.printStackTrace();
+            int i = 0;
+            for(Course c:listOfCourses){
+                i++;
+                System.out.println(i+". "+c.getCode()+" "+c.getName());
+            }
+            if (scan.next() == "1"){
+                affichageDesCours(os,is,scan);
+            }
         }
 
     }
+
+    public static void main(String[] args){
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 1337);
+            ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
+            Scanner scan = new Scanner(System.in);
+
+            affichageDesCours(os,is,scan);
+
+        }catch (IOException ex){
+            ex.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 
 }
