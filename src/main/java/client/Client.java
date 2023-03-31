ada7febbe1e6a7b0e54ab108b02860f74d2def5a
Jhosim-agudelo <jhosim.alejandro.agudelo.campo@umontreal.ca>
2023-03-31 15:07:56 -0400

    tache 2 F1 version 2

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -11,17 +11,22 @@ import java.util.Scanner;
 public class Client {
 
 
-    static String messagePrincipal = "*** Bienvenue au portail d'inscription de cours de l'UDEM***\n" +
+    static final String messagePrincipal = "*** Bienvenue au portail d'inscription de cours de l'UDEM***\n" +
             "Veuillez choisir la session pour laquelle vous voulez consulter la liste des cours:\n" +
-            "1. Automne\n2. Hiver\n3. Ete";
+            "1. Automne\n2. Hiver\n3. Ete \n>Choix: ";
+
+    static final String messageSecondaire = "Choix: \n1. Consulter les cours offerts pour une autre session" +
+            "\n2. Inscription à un cours\n>Choix: ";
 
 
-    public static void affichageDesCours(ObjectOutputStream os,
-                                  ObjectInputStream is,
-                                  Scanner scan) throws IOException, ClassNotFoundException {
-        System.out.println(messagePrincipal);
+    public static void affichageDesCours(Scanner scan) throws IOException, ClassNotFoundException {
 
-        while (scan.hasNext()) {
+        Socket clientSocket = new Socket("127.0.0.1", 1337);
+        ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
+
+
+
             String answer = scan.nextLine();
             switch (answer) {
                 case "1":
@@ -44,28 +49,43 @@ public class Client {
                 i++;
                 System.out.println(i+". "+c.getCode()+" "+c.getName());
             }
-            if (scan.next() == "1"){
-                affichageDesCours(os,is,scan);
-            }
-        }
+
 
     }
 
+    public static void inscriptionCours() throws IOException {
+        Socket clientSocket = new Socket("127.0.0.1", 1337);
+        ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
+
+
+
+    }
+
+
     public static void main(String[] args){
         try {
-            Socket clientSocket = new Socket("127.0.0.1", 1337);
-            ObjectOutputStream os = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream is = new ObjectInputStream(clientSocket.getInputStream());
+            System.out.print(messagePrincipal);
             Scanner scan = new Scanner(System.in);
+            while (scan.hasNext()){
 
-            affichageDesCours(os,is,scan);
+                affichageDesCours(scan);
+                System.out.print(messageSecondaire);
+
+                String followingAnswer = scan.nextLine();
+                if (followingAnswer.equals("1") ){
+                    main(args);
+                }else{
+                    System.out.println("on va s'inscrire");
+                }
+            }
+            scan.close();
 
         }catch (IOException ex){
             ex.printStackTrace();
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-
     }
 
 
